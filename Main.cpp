a53f515990655bc49751bead8ae49d21b27796cd
cf16 <cf16rocket@gmail.com>
2013-05-18 02:47:39 +0200

    Initial base for a trading client. It includes common includes for TWS API: /shared /PosixSocketClient/src

---
+++ Main.cpp
@@ -0,0 +1,44 @@
+#ifdef _WIN32
+# include <windows.h>
+# define sleep( seconds) Sleep( seconds * 1000);
+#else
+# include <unistd.h>
+#endif
+
+#include "PosixTestClient.h"
+
+const unsigned MAX_ATTEMPTS = 1;
+const unsigned SLEEP_TIME = 10;
+
+int main(int argc, char** argv)
+{
+	const char* host = argc > 1 ? argv[1] : "";
+	unsigned int port = argc > 2 ? atoi(argv[2]) : 7496;
+	int clientId = 7;
+
+	unsigned attempt = 0;
+	printf( "Start of POSIX Socket Client2 Test %u\n", attempt);
+
+	for (;;) {
+		++attempt;
+		printf( "Attempt %u of %u\n", attempt, MAX_ATTEMPTS);
+
+		IB::PosixTestClient client;
+
+		client.connect( host, port, clientId);
+
+		while( client.isConnected()) {
+			client.processMessages();
+		}
+
+		if( attempt >= MAX_ATTEMPTS) {
+			break;
+		}
+
+		printf( "Sleeping %u seconds before next attempt\n", SLEEP_TIME);
+		sleep( SLEEP_TIME);
+	}
+
+	printf ( "End of POSIX Socket Client Test\n");
+}
+
---
+++ build/Debug/GNU_4.7-Linux-x86/Main.o.d
@@ -0,0 +1,11 @@
+build/Debug/GNU_4.7-Linux-x86/Main.o: Main.cpp PosixTestClient.h \
+ ../twsapi/Shared/EWrapper.h ../twsapi/Shared/CommonDefs.h \
+ ../twsapi/Shared/IBString.h
+
+PosixTestClient.h:
+
+../twsapi/Shared/EWrapper.h:
+
+../twsapi/Shared/CommonDefs.h:
+
+../twsapi/Shared/IBString.h:
